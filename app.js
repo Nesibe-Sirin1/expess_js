65141e5dc01d0b62099398b22b87ad4c11cebeeb
42 Nesibe <nesibesahinrte2023@gmail.com>
2021-05-12 01:44:50 +0300

    error-sayfası

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.use('/admin',adminRoutes);
 const userRoutes=require('./routes/user');
 app.use(userRoutes);
 
-
+app.use((req,res) =>{
+    res.status(404);
+    res.send('<h1> Page Not Found </h1>');
+});
 
 app.listen(3000,()=>{
     console.log("listening on port 3000");

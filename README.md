87ed84d444bd3207350647229e146fb51ad9ad8c
42 Nesibe <nesibesahinrte2023@gmail.com>
2021-05-12 01:01:07 +0300

    routing

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1-express kurulumu
     npm install express
 2-middleware kullanımı
+3-Routing
--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('middleware 1 çalıştırıldı');
+app.use('/',(req,res,next)=>{
+    console.log('loglama yapıldı');
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log('middleware 2 çalıştırıldı');
-    res.send('<h1>hello from express </h1>');
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1> adding  product page </h1>');
+});
+
+app.use('/product-list',(req,res,next)=>{
+    res.send('<h1>product list page</h1>');
 });
 
 app.listen(3000,()=>{
